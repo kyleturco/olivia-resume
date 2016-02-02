0bb6ff2ea0b0367490165b46d03c136dfdc8d411
Kyle Turco <kyleturco@gmail.com>
2016-02-02 01:16:17 -0600

    update modal styles, clean up photo classes

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function ready() {
 
   // tells the modal which photo to open
   function photoModal() {
-    $('.photo-thumb img').on('click', function(e) {
+    $('.photo-thumb').on('click', function(e) {
       var dataUrl = $(this).attr('src');
       $('#photoModal img').attr('src', $(this).attr('src'));
     });
