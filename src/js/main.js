2445bab01b0191ab0745ca07cc9b2a280e9de1f6
Kyle Turco <kyleturco@gmail.com>
2016-02-02 00:51:43 -0600

    add modal and photo grabbing logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function ready() {
 
   function init() {
-
+    photoModal();
   }
+
+  // tells the modal which photo to open
+  function photoModal() {
+    $('.photo-thumb img').on('click', function(e) {
+      var dataUrl = $(this).attr('src');
+      $('#photoModal img').attr('src', $(this).attr('src'));
+    });
+  };
 
   init();
 });
