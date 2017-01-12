76c615dfc9dc8565cb6133857544d9d69b1ddd1f
Kyle Turco <kyleturco@gmail.com>
2017-01-11 22:54:36 -0600

    refactor(JS): move photoThumb out of init

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,10 @@
 $(document).ready(function ready() {
 
-  function init() {
-    photoModal()
-  }
-
   var $photoThumb = $('.photo-thumb')
   var $modalImg = $('#photoModal img')
 
-  // tells the modal which photo to open
-  function photoModal() {
+  function init() {
+    // tells the modal which photo to open
     $photoThumb.on('click', function(e) {
       var dataUrl = $(this).attr('src')
       $modalImg.attr('src', dataUrl)
