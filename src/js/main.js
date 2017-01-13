bace2ef2638a4233d3fe1d0f0f0e419435c21b4f
Kyle Turco <kyleturco@gmail.com>
2017-01-13 00:03:33 -0600

    feat(Nav): update tabs and add hash functionality

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,18 @@ $(document).ready(function ready() {
   var $photoThumb = $('.photo-thumb')
   var $modalImg = $('#photoModal img')
 
+  var $navbarLink = $('.navbar__link')
+
   function init() {
+
+    var hash = window.location.hash
+    hash && $('a.navbar__link[href="' + hash + '"]').tab('show')
+
+    $navbarLink.click(function(e) {
+      $(this).tab('show')
+      window.location.hash = this.hash;
+    })
+
     // tells the modal which photo to open
     $photoThumb.on('click', function(e) {
       var dataUrl = $(this).attr('src')
@@ -13,3 +24,17 @@ $(document).ready(function ready() {
 
   init()
 })
+
+
+
+// $(function(){
+//   var hash = window.location.hash;
+//   hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+//
+//   $('.nav-tabs a').click(function (e) {
+//     $(this).tab('show');
+//     var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+//     window.location.hash = this.hash;
+//     $('html,body').scrollTop(scrollmem);
+//   });
+// });
